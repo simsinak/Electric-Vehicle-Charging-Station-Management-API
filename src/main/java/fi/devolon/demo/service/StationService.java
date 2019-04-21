218d0ebf971e9b5ff4f38aac4aa969ccdccf61c8
sinaaskarnejad <curse_dmc4@yahoo.com>
2019-04-21 19:32:52 +0430

    fix a bug in update method which did not changed the company

--- src/main/java/fi/devolon/demo/service/StationService.java
+++ src/main/java/fi/devolon/demo/service/StationService.java
@@ -6,17 +6,21 @@ import fi.devolon.demo.model.Station;
 import fi.devolon.demo.repository.StationRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.Optional;
-
+@Service
 @AllArgsConstructor(onConstructor = @__(@Autowired))
 public class StationService implements BasicRestService<Station> {
 
     StationRepository stationRepository;
     CompanyService companyService;
     @Override
-    public Station save(Station company) {
-        return stationRepository.save(company);
+    public Station save(Station station) {
+        Company company=companyService.getEntityByID(station.getCompany().getId());
+        company.getStations().add(station);
+        station.setCompany(company);
+        return stationRepository.save(station);
     }
 
     @Override
@@ -42,6 +46,10 @@ public class StationService implements BasicRestService<Station> {
             stn.setLongitude(station.getLongitude());
             Company company=companyService.getEntityByID(station.getCompany().getId());
             stn.setCompany(company);
+            company.getStations().add(stn);
+            System.out.println(stn.getName());
+            System.out.println(stn.getLongitude());
+
             return stationRepository.save(stn);})
                 .orElseThrow(()-> new StationNotFoundException(id));
     }
